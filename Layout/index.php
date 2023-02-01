e72faad4f4508742c33082570145970b343952ba
AdamKessa <adam22000@student.hamk.fi>
2023-02-01 10:57:32 +0200

    nothinf

--- Layout/index.php
+++ Layout/index.php
@@ -1,5 +1,5 @@
 <?php include "header.php" ?>
 
 <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores magnam, error beatae, nihil quis culpa rerum ut cupiditate officiis odio voluptates iure. Ex, magni magnam quisquam debitis modi quas vitae.</h1>
-
-<?php include "footer.php" ?>
+<p>empty</p>
+<?php include "footer.php" ?> ?
