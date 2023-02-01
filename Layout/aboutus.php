d7b20834a406efcf8f4fd4c977e62e75bbe7905c
AdamKessa <adam22000@student.hamk.fi>
2023-02-01 10:29:50 +0200

    Initial commit

---
+++ Layout/aboutus.php
@@ -0,0 +1,8 @@
+<?php
+$title = "lol";
+include "header.php" ?>
+
+<h2>Contact Us</h2>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat, et placeat. Consectetur sequi, quo, officia ut hic voluptatum reiciendis quos quibusdam veniam facere officiis iste commodi, saepe minima quam voluptatem.</p>
+
+<?php include "footer.php" ?>
---
+++ Layout/ex1.php
@@ -0,0 +1,27 @@
+<?php include "Layout/header.php" ?>
+    <h1>3.1 Write a simple PHP script to print your information</h1>
+<?php
+//opening tag
+
+echo "Adam Kessa";
+echo "BBCAP22";
+
+
+?> 
+<h1>3.2 Hello world! My name is "David"</h1>
+<?php
+//opening tag
+echo "Hello World! My name is \"David\" ";
+
+?> 
+
+<h1>3.3 Current Date</h1>
+<?php
+
+echo date("d.m.Y")
+
+?>
+<?php include "Layout/footer.php" ?>
+
+
+
